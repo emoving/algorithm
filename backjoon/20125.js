b3e9343b0b30d282cd1c50ee188a494e25b4cc6b
이동주 <leednj0113@naver.com>
2023-11-19 16:06:34 +0900

    20125

---
+++ backjoon/20125.js
@@ -0,0 +1,58 @@
+/**
+ * 쿠키의 신체 측정
+ *
+ * 2차원 평면에서 범위 내 조건 만족하는 길이 구하는 문제
+ */
+const input = require("fs")
+  .readFileSync(process.platform == "linux" ? 0 : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = Number(input[0]);
+const arr = Array.from(Array(n + 1), () => Array(n + 1));
+let heart;
+let flag = false;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    arr[i][j] = input[i][j - 1];
+
+    if (!flag && arr[i][j] === "*") {
+      heart = [i + 1, j];
+      flag = true;
+    }
+  }
+}
+
+const ans = new Array(5).fill(0);
+
+// 왼쪽 팔
+for (let i = heart[1] - 1; i > 0; i--) {
+  if (arr[heart[0]][i] !== "*") break;
+
+  ans[0]++;
+}
+
+// 오른쪽 팔
+for (let i = heart[1] + 1; i <= n; i++) {
+  if (arr[heart[0]][i] !== "*") break;
+
+  ans[1]++;
+}
+
+// 허리
+for (let i = heart[0] + 1; i <= n; i++) {
+  if (arr[i][heart[1]] !== "*") break;
+
+  ans[2]++;
+}
+
+// 다리
+for (let i = heart[0] + ans[2] + 1; i <= n; i++) {
+  if (arr[i][heart[1] - 1] === "*") ans[3]++;
+  if (arr[i][heart[1] + 1] === "*") ans[4]++;
+}
+
+console.log(heart.join(" "));
+console.log(ans.join(" "));
--- main.js
+++ main.js
@@ -3,35 +3,3 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n");
-
-const [n, k] = input[0].split(" ").map(Number);
-const arr = input[1].split("");
-
-let cnt = 0;
-P: for (let i = 0; i < n; i++) {
-  if (arr[i] === "P") {
-    // 왼쪽 먼저 탐색
-    for (let j = k; j >= 1; j--) {
-      if (i < j) continue;
-
-      if (arr[i - j] === "H") {
-        cnt++;
-        arr[i - j] = "";
-        continue P;
-      }
-    }
-
-    // 오른쪽 탐색
-    for (let j = 1; j <= k; j++) {
-      if (i + j >= n) continue;
-
-      if (arr[i + j] === "H") {
-        cnt++;
-        arr[i + j] = "";
-        continue P;
-      }
-    }
-  }
-}
-
-console.log(cnt);
