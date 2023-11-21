6e60bdbf1a6ccc853258b721249f549f4055fb05
이동주 <leednj0113@naver.com>
2023-11-21 18:01:03 +0900

    Revert "2-118667-두큐합같게만들기(큐 구현)"
    This reverts commit 716327d0e22b70ac957fe2e8c802c9ee14b1f327.

--- programmers/2-118667-두큐합같게만들기(큐 구현).js
+++ programmers/2-118667-두큐합같게만들기(큐 구현).js
@@ -1,62 +1,24 @@
-class Que {
-  constructor() {
-    this.map = new Map();
-    this.first = 0;
-    this.last = 0;
-  }
-
-  add(e) {
-    if (!this.map.size) {
-      this.map.set(0, e);
-    } else {
-      this.last += 1;
-      this.map.set(this.last, e);
-    }
-  }
-
-  poll() {
-    const e = this.map.get(this.first);
-
-    if (this.map.size == 1) {
-      this.map.clear();
-      this.first = 0;
-      this.last = 0;
-    } else {
-      this.map.delete(this.first);
-      this.first += 1;
-    }
-
-    return e;
-  }
-
-  size() {
-    return this.map.size();
-  }
-}
-
-function solution(queue1, queue2) {
-  let que1Sum = queue1.reduce((sum, e) => sum + e, 0);
-  let que2Sum = queue2.reduce((sum, e) => sum + e, 0);
+function solution(que1, que2) {
+  let que1Sum = que1.reduce((sum, e) => sum + e, 0);
+  let que2Sum = que2.reduce((sum, e) => sum + e, 0);
 
   if ((que1Sum + que2Sum) % 2 !== 0) return -1;
 
-  const que1 = new Que();
-  const que2 = new Que();
+  const length = que1.length + que2.length;
+  let que1Idx = 0;
+  let que2Idx = 0;
 
-  queue1.forEach((e) => que1.add(e));
-  queue2.forEach((e) => que2.add(e));
-
-  for (let i = 0, cnt = 0; i < queue1.length * 3; i++, cnt++) {
+  for (let i = 0, cnt = 0; i < length * 2; i++, cnt++) {
     if (que1Sum > que2Sum) {
-      const n = que1.poll();
-      que2.add(n);
+      const n = que1[que1Idx++];
 
+      que2.push(n);
       que1Sum -= n;
       que2Sum += n;
     } else if (que1Sum < que2Sum) {
-      const n = que2.poll();
-      que1.add(n);
+      const n = que2[que2Idx++];
 
+      que1.push(n);
       que2Sum -= n;
       que1Sum += n;
     } else {
