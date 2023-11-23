0ca8913697de213d61bf3e84e946663492075fc4
이동주 <leednj0113@naver.com>
2023-11-23 17:47:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const input = require("fs")
-  .readFileSync(process.platform == "linux" ? 0 : "input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+class Node {
+  constructor(idx, prev) {
+    this.idx = idx;
+    this.prev = prev;
+    this.next = null;
+  }
+}
+
+console.log(new Node(0));
+
+function solution(n, k, cmd) {
+  let prevNode = new Node(0);
+  let curNode;
+
+  for (let i = 1; i < n; i++) {
+    const newNode = new Node(i, prevNode);
+    prevNode.next = newNode;
+    prevNode = newNode;
+
+    if (i === k) curNode = newNode;
+
+    cmd.forEach((command) => {});
+  }
+}
+
+solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"]);
